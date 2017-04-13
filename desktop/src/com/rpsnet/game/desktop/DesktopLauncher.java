76cf980a900919b32413002dd0d602f14fb33ade
Michael Walsh <Michaelwalsh223@hotmail.co.uk>
2017-04-13 20:19:43 +0100

    Removed texturepacker now that the atlas is complete

--- desktop/src/com/rpsnet/game/desktop/DesktopLauncher.java
+++ desktop/src/com/rpsnet/game/desktop/DesktopLauncher.java
@@ -3,12 +3,11 @@ package com.rpsnet.game.desktop;
 import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.rpsnet.game.RPSNet;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		TexturePacker.process("UI-Raw", "UI", "uiElements");
+		//TexturePacker.process("UI-Raw", "UI", "uiElements");
 
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
